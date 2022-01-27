cfb6a5db3a9d39a92e1cf314bc11b048d36bf790
mikoto <mikoto@ungine.cn>
2022-01-27 21:50:43 +0800

    [init](pixiv-database): Init repo

---
+++ src/main/java/net/mikoto/pixiv/database/PixivDatabaseApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.pixiv.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/pixiv/database/PixivDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.pixiv.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
