34d90349798aff7f0547cba0803f622fb57b59bc
mikoto <mikoto@ungine.cn>
2022-05-28 11:33:54 +0800

    [fix] 修正异常处理

--- src/main/java/net/mikoto/pixiv/database/controller/ArtworkController.java
+++ src/main/java/net/mikoto/pixiv/database/controller/ArtworkController.java
@@ -3,6 +3,9 @@ package net.mikoto.pixiv.database.controller;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
+import net.mikoto.pixiv.api.http.database.artwork.GetArtwork;
+import net.mikoto.pixiv.api.http.database.artwork.GetArtworks;
+import net.mikoto.pixiv.api.http.database.artwork.InsertArtworks;
 import net.mikoto.pixiv.api.model.Artwork;
 import net.mikoto.pixiv.database.service.ArtworkService;
 import org.jetbrains.annotations.NotNull;
@@ -11,19 +14,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
-import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
 import static net.mikoto.pixiv.api.http.HttpApi.*;
@@ -40,7 +37,7 @@ import static net.mikoto.pixiv.database.constant.Constant.*;
 @RequestMapping(
         DATABASE_ARTWORK
 )
-public class ArtworkController {
+public class ArtworkController implements InsertArtworks, GetArtwork, GetArtworks {
     @Qualifier("artworkService")
     private final ArtworkService artworkService;
 
@@ -55,7 +52,8 @@ public class ArtworkController {
             value = DATABASE_ARTWORK_INSERT_ARTWORKS,
             method = RequestMethod.POST
     )
-    public void insertArtworks(@RequestBody @NotNull String data) {
+    @Override
+    public void insertArtworksHttpApi(@RequestBody @NotNull String data) {
         JSONObject jsonObject = JSON.parseObject(new String(Base64.getDecoder().decode(data), StandardCharsets.UTF_8));
         if (jsonObject.getString(KEY).equals(MAIN_PROPERTIES.getProperty(ADMIN_KEY)) || jsonObject.getString(KEY).equals(MAIN_PROPERTIES.getProperty(ACCESS_KEY))) {
             for (Object artworkJson :
@@ -69,28 +67,33 @@ public class ArtworkController {
     @RequestMapping(
             DATABASE_ARTWORK_GET_ARTWORK
     )
-    public JSONObject getArtwork(@NotNull HttpServletResponse response, Integer artworkId) throws NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, InvalidKeyException {
+    @Override
+    public JSONObject getArtworkHttpApi(@NotNull HttpServletResponse response, int artworkId) {
         response.setContentType("application/json;charset=UTF-8");
         JSONObject jsonObject = new JSONObject();
         Artwork artwork = artworkService.getArtworkByArtworkId(artworkId);
         if (artwork != null) {
-            jsonObject.fluentPut("body", artwork);
-            jsonObject.fluentPut("sign", sign(getSha256(jsonObject.getJSONObject("body").toJSONString()), getPrivateKey(MAIN_PROPERTIES.getProperty(RSA_PRIVATE_KEY))));
-            jsonObject.fluentPut("success", true);
-            jsonObject.fluentPut("message", "");
+            try {
+                jsonObject.fluentPut("body", artwork);
+                jsonObject.fluentPut("sign", sign(getSha256(jsonObject.getJSONObject("body").toJSONString()), getPrivateKey(MAIN_PROPERTIES.getProperty(RSA_PRIVATE_KEY))));
+                jsonObject.fluentPut("success", true);
+            } catch (Exception e) {
+                jsonObject.fluentPut("success", false);
+                jsonObject.fluentPut("message", e.getMessage());
+            }
         } else {
-            jsonObject.fluentPut("body", null);
-            jsonObject.fluentPut("sign", null);
             jsonObject.fluentPut("success", false);
-            jsonObject.fluentPut("message", "Null artwork");
+            jsonObject.fluentPut("message", "No such artwork.");
         }
         return jsonObject;
     }
 
     @RequestMapping(
             DATABASE_ARTWORK_GET_ARTWORKS
+
     )
-    public JSONObject getArtworks(@NotNull HttpServletResponse response, String credential, Sort.Direction order, @NotNull String properties, int pageCount) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    @Override
+    public JSONObject getArtworksHttpApi(@NotNull HttpServletResponse response, String credential, Sort.Direction order, @NotNull String properties, int pageCount) {
         response.setContentType("application/json;charset=UTF-8");
         Page<Artwork> artworkList;
 
@@ -101,15 +104,14 @@ public class ArtworkController {
                     credential,
                     PageRequest.of(pageCount, 10, order, properties.split(";"))
             );
-        } catch (PropertyReferenceException e) {
+        } catch (Exception e) {
             jsonObject.fluentPut("success", false);
-            jsonObject.fluentPut("body", null);
+            jsonObject.fluentPut("message", e.getMessage());
             return jsonObject;
         }
 
         if (artworkList == null || artworkList.isEmpty()) {
             jsonObject.fluentPut("success", false);
-            jsonObject.fluentPut("body", null);
             return jsonObject;
         }
 
